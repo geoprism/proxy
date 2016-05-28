6581418b16e45fc33311a50037765f418765e1b9
Jason <silentspeedz@gmail.com>
2016-05-27 23:39:09 -0700

    wat

--- echoserver.c
+++ echoserver.c
@@ -1,30 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
 #include "csapp.h"
 
-int main(int argc, char **argv) {
-    int listenfd, connfd, clientlen;
-    char *port;
-    struct sockaddr_in clientaddr;
-    struct hostent *hp;
-    char *haddrp;
-    unsigned short client_port;
+void echo(int connfd)
+{
+  size_t n;
+  char buf[MAXLINE];
+  rio_t rio;
+  Rio_readinitb(&rio, connfd);
+  while((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) {
+    printf("server received %zu bytes\n", n);
+    Rio_writen(connfd, buf, n);
+  }
+}
 
-    port = argv[1]; /* the server listens on a port passed
-                             on the command line */
-    listenfd = open_listenfd(port);
 
-    while (1) {
-        clientlen = sizeof(clientaddr);
-        connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
-        hp = Gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr,
-                        sizeof(clientaddr.sin_addr.s_addr), AF_INET);
-        haddrp = inet_ntoa(clientaddr.sin_addr);
-        client_port = ntohs(clientaddr.sin_port);
-        printf("server connected to %s (%s), port %u\n",
-                hp->h_name, haddrp, client_port);
-        //echo(connfd);
-        Close(connfd);
-    }
+
+int main(int argc, char **argv)
+{
+  int listenfd, connfd, clientlen;
+  char *port;
+  struct sockaddr_in clientaddr;
+  struct hostent *hp;
+  char *haddrp;
+  if (argc != 2) {
+    fprintf(stderr, "usage: %s <port>\n", argv[0]);
+    exit(0);
+  }
+  port = argv[1];
+
+  listenfd = Open_listenfd(port);
+  while (1) {
+    clientlen = sizeof(clientaddr);
+    connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+
+    /* Determine the domain name and IP address of the client */
+    hp = Gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, sizeof(clientaddr.sin_addr.s_addr), AF_INET);
+    haddrp = inet_ntoa(clientaddr.sin_addr);
+    printf("server connected to %s (%s)\n", hp->h_name, haddrp);
+
+    echo(connfd);
+    Close(connfd);
+  }
+  exit(0);
 }
